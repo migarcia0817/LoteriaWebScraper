11c0b5d1bdac0b622d33a51d86669bd847b54045
migarcia0817 <migarcia0817@gmail.com>
2026-04-16 23:17:50 -0400

    Fix fecha con FechaHelper

--- LoteriaWebScraper/FechaHelper.cs
+++ LoteriaWebScraper/FechaHelper.cs
@@ -1,32 +1,22 @@
-﻿namespace LoteriaWebScraper
+﻿public static class FechaHelper
 {
-    public static class FechaHelper
+    private static readonly TimeZoneInfo SantoDomingoTZ = GetTimeZone();
+
+    private static TimeZoneInfo GetTimeZone()
     {
-        private static readonly TimeZoneInfo SantoDomingoTZ = GetTimeZone();
-
-        private static TimeZoneInfo GetTimeZone()
+        try
         {
-            try
-            {
-                // Linux / Render usa IANA
-                return TimeZoneInfo.FindSystemTimeZoneById("America/Santo_Domingo");
-            }
-            catch (TimeZoneNotFoundException)
-            {
-                // Windows usa Registry
-                return TimeZoneInfo.FindSystemTimeZoneById("SA Western Standard Time");
-            }
+            return TimeZoneInfo.FindSystemTimeZoneById("America/Santo_Domingo"); // Render/Linux
         }
-
-        public static string GetFechaLocal()
+        catch (TimeZoneNotFoundException)
         {
-            var localTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, SantoDomingoTZ);
-            return localTime.ToString("yyyy-MM-dd");
+            return TimeZoneInfo.FindSystemTimeZoneById("SA Western Standard Time"); // Windows
         }
+    }
 
-        public static DateTime GetDateTimeLocal()
-        {
-            return TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, SantoDomingoTZ);
-        }
+    public static string GetFechaLocal()
+    {
+        var localTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, SantoDomingoTZ);
+        return localTime.ToString("yyyy-MM-dd");
     }
 }
